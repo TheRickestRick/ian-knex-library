d53f3d382accb8af87dc3fdb15854cf1e4954e1c
Timothy <tbarnes62@austin.rr.com>
2018-04-10 13:45:37 -0500

    Addedknex instance that connects to the DB and shiz

---
+++ routes/authors.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('../db/knex');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
