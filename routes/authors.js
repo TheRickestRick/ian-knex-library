2a0cc66b4357674d2c674e653670838168440b52
Timothy <tbarnes62@austin.rr.com>
2018-04-10 14:16:50 -0500

    Ready to test route

--- routes/authors.js
+++ routes/authors.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-
+router.get('/authors', (req, res)=>{
+    knex('authors')
+    .select('*')
+    .then((data)=>{
+      res.json(data);
+    });
+});
 
 
 
