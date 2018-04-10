b44a966c6fd8260be165fbbc906cd6c0c8f30912
Timothy <tbarnes62@austin.rr.com>
2018-04-10 13:29:40 -0500

    meeeeow

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'knexlibrary_dev'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'knexlibrary_test'
+    }
+  }
+
+};
