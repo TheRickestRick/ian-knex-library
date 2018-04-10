2d800ef58fd386103abe7e30c9feabccc4c2ad27
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-10 14:07:51 -0500

    Initial server setup

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const authors = require('./routes/authors');
+var PORT = process.env.port || 8080;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/authors', authors);
+
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+	console.log(`Hokay! We are rolling on port ${PORT}`);
+});
 
 module.exports = {
-  app
-}
+	app
+};
