1f1a6c542b81590ac258decbb5351f3ca0384dde
Timothy <tbarnes62@austin.rr.com>
2018-04-10 16:14:00 -0500

    GET Routes added and a PUT and a DELETE method route added

--- routes/authors.js
+++ routes/authors.js
@@ -5,15 +5,31 @@ const knex = require('../db/knex');
 
 
 router.get('/', (req, res)=>{
-    knex('authors')
+  knex('authors')
     .then((authors)=>{
       res.render('authors/index', {authors});
     });
 });
 
+router.get('/:id', (req, res)=>{
+  knex('authors')
+    .where('id', req.params.id)
+    .then((author)=>{
+      res.render('authors/single', {author})
+    });
+});
+
+router.get('/:id/edit', (req, res)=>{
+  knex('authors')
+  .where('id', req.params.id)
+  .then((author)=>{
+    res.render('authors/edit', {author})
+  });
+});
+
 router.get('/new', (req, res)=>{
   res.render('authors/new');
-})
+});
 
 
 
@@ -24,6 +40,24 @@ router.post('/', (req, res)=>{
     res.redirect('/authors')
   });
 });
+
+router.put('/:id', (req, res)=>{
+  knex('authors')
+  .where('id', req.params.id)
+  .insert(req.body)
+  .then((author)=>{
+    res.redirect('/authors')
+  });
+});
+
+router.delete('/:id', (req, res)=>{
+  knex('authors')
+  .where('id', req.params.id)
+  .del()
+  .then(()=>{
+    res.redirect('/authors')
+  })
+})
 
 
 
